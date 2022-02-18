52045fb1b2743ef4962dedfafe9e9853348a0265
JonR98 <joni.repo@student.hamk.fi>
2022-02-18 11:01:47 +0200

    .

--- palautetallennus.php
+++ palautetallennus.php
@@ -1,34 +1,27 @@
 
 
 <?php 
-
+$nimi="";
 if (isset($_POST["nimi"])){
    $nimi=$_POST["nimi"];
 }
-else{
-  
-   header("Location:ruokalomake.html");
-   exit;
-}
+
+$asiakaspalvelu="";
 if (isset($_POST["asiakaspalvelu"])){
-   $nimi=$_POST["asiakaspalvelu"];
+   $asiakaspalvelu=$_POST["asiakaspalvelu"];
 }
-else{
-   $asiakaspalvelu="";
-}
+$ruoka=""; 
 if (isset($_POST["ruoka"])){
-   $nimi=$_POST["ruoka"];
-}
-else{
-   $ruoka="";
-if (isset($_POST["Vapaapalaute"])){
-   $nimi=$_POST["Vapaapalaute"];
-}
-else{
-   $Vapaapalaute="";
-}
+   $ruoka=$_POST["ruoka"];
 
 }
+$vapaapalaute="";
+if (isset($_POST["vapaapalaute"])){
+   $vapaapalaute=$_POST["vapaapalaute"];
+}
+
+
+
 
 $yhteys=mysqli_connect("db", "root", "password", "dining007");
 if (!$yhteys) {
@@ -36,14 +29,13 @@ if (!$yhteys) {
 }
 
 
-$sql="insert into palaute3(nimi, asiakaspalvelu, ruoka, Vapaapalaute) values(?, ?, ?, ?)";
+$sql="insert into palaute(nimi, asiakaspalvelu, ruoka, vapaapalaute) values(?, ?, ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, 'ssss', $nimi, $asiakaspalvelu, $ruoka, $Vapaapalaute);
+mysqli_stmt_bind_param($stmt, 'ssss', $nimi, $asiakaspalvelu, $ruoka, $vapaapalaute);
 mysqli_stmt_execute($stmt);
 
 mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
 
-header("Location:palautekiitos.html");
-exit;
+
 ?>

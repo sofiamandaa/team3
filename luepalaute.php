69701b0e2ea325148395b444c94a90f555b33a89
sofia21007 <sofia.2.makela@student.hamk.fi>
2022-02-18 12:21:19 +0200

    Update luepalaute.php

--- luepalaute.php
+++ luepalaute.php
@@ -3,7 +3,7 @@ try{
     $yhteys=mysqli_connect("db", "root", "password", "dining007");
 }
 catch(Exception $e){
-    header("Location:../html/yhteysvirhe.html");
+    header("Location:/yhteysvirhe.html");
     exit;
 }
 $tulos=mysqli_query($yhteys, "select * from palaute");

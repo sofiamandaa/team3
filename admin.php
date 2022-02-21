487fb333096378cca8018439f8b6f312d066a7ed
janican <nieminenjanica@gmail.com>
2022-02-21 21:24:52 +0200

    q

--- admin.php
+++ admin.php
@@ -1,27 +1,14 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ERROR ^ MYSQLI_REPORT_STRICT);
+include "./header.html"
+?>
 
-$poistettava=isset($_GET["poistettava"]) ? $_GET["poistettava"] : 0;
-
-if (empty($poistettava)){
-    header("Location:./luepalaute.php");
-    exit;
-}
+<?php
 
 try{
     $yhteys=mysqli_connect("db", "root", "password", "dining007");
 }
 catch(Exception $e){
-    header("Location:./yhteysvirhe.html");
-    exit;
-}
-$sql="delete from palaute where id=?";
-
-try{
-    $yhteys=mysqli_connect("db", "root", "password", "dining007");
-}
-catch(Exception $e){
-    print "yhteysvirhe";
+    header("location:./yhteysvirhe.html");
     exit;
 }
 $tulos=mysqli_query($yhteys, "select * from palaute");
@@ -29,17 +16,15 @@ $tulos=mysqli_query($yhteys, "select * from palaute");
 print "<ol>";
 while ($rivi=mysqli_fetch_object($tulos)){
     print "<li>$rivi->nimi $rivi->asiakaspalvelu $rivi->ruoka"."
-    <a href='./luepalaute.php?poistettava=$rivi->nimi'>Poista</a></li>";
+    <a href='./poista.php?poistettava=$rivi->nimi'>Poista</a></li>";
 }
 print "</ol>";
 
-$stmt=mysqli_prepare($yhteys, $sql);
-Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'i', $poistettava);
-Suoritetaan sql-lause
-mysqli_stmt_execute($stmt);
-
 mysqli_close($yhteys);
 
-header("Location:./luepalaute.php");
+
+?>
+
+<?php
+include "./footer.html"
 ?>

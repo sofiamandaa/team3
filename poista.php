c1f9a1741e5b25e5cb4903415294116cc2606d0d
janican <nieminenjanica@gmail.com>
2022-02-22 09:17:09 +0200

    .

--- poista.php
+++ poista.php
@@ -20,7 +20,7 @@ $sql="delete from palaute where nimi=?";
 
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'ss', $poistettava, $muokattava);
+mysqli_stmt_bind_param($stmt, 's', $poistettava);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 
